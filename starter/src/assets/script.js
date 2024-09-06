9df72442aaf109f744680cf87a39272247985d73
Daniel <dannyb829@gmail.com>
2024-09-06 18:02:34 -0400

    semi final commit, maybe some refactoring

--- starter/src/assets/script.js
+++ starter/src/assets/script.js
@@ -1,13 +1,31 @@
 /* Create an array named products which you will use to add all of your product object literals that you create in the next step. */
 
 /* Create 3 or more product objects using object literal notation 
-   Each product should include five properties
-   - name: name of product (string)
-   - price: price of product (number)
-   - quantity: quantity in cart should start at zero (number)
-   - productId: unique id for the product (number)
-   - image: picture of product (url string)
+Each product should include five properties
+- name: name of product (string)
+- price: price of product (number)
+- quantity: quantity in cart should start at zero (number)
+- productId: unique id for the product (number)
+- image: picture of product (url string)
 */
+
+const products = [
+  {name: "cherry",
+  price: 2.50,
+quantity: 0,
+productId: 1,
+image: "./images/cherry.jpg"},
+  {name: "orange",
+  price: 4.50,
+quantity: 0,
+productId: 2,
+image: "./images/orange.jpg"},
+  {name: "strawberry",
+  price: 6.50,
+quantity: 0,
+productId: 3,
+image: "./images/strawberry.jpg"}
+]
 
 /* Images provided in /images folder. All images from Unsplash.com
    - cherry.jpg by Mae Mu
@@ -17,45 +35,139 @@
 
 /* Declare an empty array named cart to hold the items in the cart */
 
+let cart = []
+
+// value of exchange rate for currency()
+let exchangeRate = 1
+
 /* Create a function named addProductToCart that takes in the product productId as an argument
   - addProductToCart should get the correct product based on the productId
   - addProductToCart should then increase the product's quantity
   - if the product is not already in the cart, add it to the cart
 */
 
+const addProductToCart = (id) => {
+  for (const product of products) {
+
+      if (product.productId === id) {
+       if (product.quantity === 0) {
+        cart.push(product)
+        product.quantity++ 
+        }
+       else {
+          product.quantity++
+        }
+      }
+
+  }
+}
+
 /* Create a function named increaseQuantity that takes in the productId as an argument
   - increaseQuantity should get the correct product based on the productId
   - increaseQuantity should then increase the product's quantity
 */
 
-/* Create a function named decreaseQuantity that takes in the productId as an argument
-  - decreaseQuantity should get the correct product based on the productId
-  - decreaseQuantity should decrease the quantity of the product
-  - if the function decreases the quantity to 0, the product is removed from the cart
-*/
+const increaseQuantity = (id) => {
+  for (const product of products) {
+    if (product.productId === id) {
+        product.quantity++
+      }
+    }
+}
 
-/* Create a function named removeProductFromCart that takes in the productId as an argument
+/* Create a function named decreaseQuantity that takes in the productId as an argument
+- decreaseQuantity should get the correct product based on the productId
+- decreaseQuantity should decrease the quantity of the product
+- if the function decreases the quantity to 0, the product is removed from the cart
+*/
+const decreaseQuantity = (id) => {
+  for (const product of products) {
+    if (product.productId === id) {
+        product.quantity -= 1
+        if (product.quantity === 0) {
+          removeProductFromCart(product.productId)
+        }
+      }
+    }
+  }
+  
+  /* Create a function named removeProductFromCart that takes in the productId as an argument
   - removeProductFromCart should get the correct product based on the productId
   - removeProductFromCart should update the product quantity to 0
   - removeProductFromCart should remove the product from the cart
-*/
+  */
+ 
+ const removeProductFromCart = (id) => {
+  for (const product of products) {
+    if (product.productId === id) {
+        product.quantity = 0
+      }
+    }
+  for (let i = 0; i < cart.length; i++) {
+    if (cart[i].productId === id) {
+        cart.splice(i,1)
+      }
+    }
+    
+  }
+  
+  /* Create a function named cartTotal that has no parameters
+  - cartTotal should iterate through the cart to get the total of all products
+  - cartTotal should return the sum of the products in the cart
+  */
+ 
+ const cartTotal = () => {
+   let total = 0
+   
+   for (const product of cart) {
+    total += (product.price * product.quantity * exchangeRate)
+  }
 
-/* Create a function named cartTotal that has no parameters
-  - cartTotal should iterate through the cart to get the total cost of all products
-  - cartTotal should return the total cost of the products in the cart
-  Hint: price and quantity can be used to determine total cost
-*/
+  return total
+}
 
 /* Create a function called emptyCart that empties the products from the cart */
 
+const emptyCart = () => {
+  for (product of products) {
+    removeProductFromCart(product.productId)
+    }
+}
+
 /* Create a function named pay that takes in an amount as an argument
-  - amount is the money paid by customer
   - pay will return a negative number if there is a remaining balance
   - pay will return a positive number if money should be returned to customer
-  Hint: cartTotal function gives us cost of all the products in the cart  
 */
 
+const pay = (amount) => {
+
+  return amount - cartTotal()
+
+}
+
+
 /* Place stand out suggestions here (stand out suggestions can be found at the bottom of the project rubric.)*/
+
+
+function currency(exchange) {
+//switch on amount value
+  switch (exchange) {
+  // rate for euro calculation
+  case 'EUR':
+    exchangeRate = .9
+    break;
+  // rate for yen calculation
+  case 'YEN':
+    exchangeRate = 143.31
+    break;
+  default:
+  // USD default
+    exchangeRate = 1
+    break;
+}
+
+
+}
 
 
 /* The following is for running unit tests. 
@@ -65,15 +177,15 @@
 */
 
 module.exports = {
-   products,
-   cart,
-   addProductToCart,
-   increaseQuantity,
-   decreaseQuantity,
-   removeProductFromCart,
-   cartTotal,
-   pay, 
-   emptyCart,
-   /* Uncomment the following line if completing the currency converter bonus */
-   // currency
+  products,
+  cart,
+  addProductToCart,
+  increaseQuantity,
+  decreaseQuantity,
+  removeProductFromCart,
+  cartTotal,
+  pay, 
+  emptyCart,
+  /* Uncomment the following line if completing the currency converter bonus */
+  currency
 }
